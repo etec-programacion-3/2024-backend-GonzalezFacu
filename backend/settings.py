07b8a8a96fcd85e3ed627e82ffd442bd5810ecc2
Facundo Gonzalez <fp.gonzalez@alumno.etec.um.edu.ar>
2024-09-12 22:33:10 -0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -100,10 +100,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gourmetkits',
+        'USER': 'FacundoGonzalez',
+        'PASSWORD': 'Facu2532',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
